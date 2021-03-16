39ce6843b856882764e06c2f2ff9a7af9bcc2cae
shayleyzerovich <58327880+shayleyzerovich@users.noreply.github.com>
2021-03-16 16:53:41 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "snowman.hpp"
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+int main() {
+	cout << ariel::snowman(12341234) << endl; 
+	cout << ariel::snowman(33232124) << endl;  
+}
